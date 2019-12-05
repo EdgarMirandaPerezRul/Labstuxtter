bb75a852c467e1372c43c18a5e91693f9dcd5cf9
Edgar Miranda perez Rul <55672259+EdgarMirandaPerezRul@users.noreply.github.com>
2019-12-04 18:26:00 -0600

    Update lab_1.sh

--- lab_1.sh
+++ lab_1.sh
@@ -1,21 +1,20 @@
 
 directory=$(pwd)
 
-instrucciones=(
-    "1. Muestra el directorio actual."     
-    "2. Crea la carpeta dir1 y muestra el resultado."     
-    "3. Crea la carpeta dir2 y muestra el resultado."     
+ejercicios=
+    "1. Muestra el directorio actual del usuario."     
+    "2. Crea la carpeta dir1 y muestra el resultado deasead."     
+    "3. Crea la carpeta dir2 y muestra el resultado  deseado."     
     "4. Crea la carpeta dir3 dentro de dir2 y muestra el resultado."     
-    "5. Crea la carpeta dir4 dentro de dir3 y muestra el resultado."     
-    "6. Lista el contenido del directorio actual."     
-    "7. Lista todos los archivos en la carpeta actual de forma\
-	recursiva."     
+    "5. Crea la carpeta dir4 dentro de dir3 y muestra el resultado xd."     
+    "6. Liste el contenido."     
+    "7. Lista todos los archivos en forma recursiva."     
     "8. Cambiarse a la carpeta dir2."     
     "9. Regresa a la carpeta anterior."     
-    "10. Cambiate a la carpeta home del usuario actual."     
-    )
+    "10. Cambiate a la carpeta home ."     
+    
 
-respuetas=(
+respuetas=
     "pwd"
     "mkdir -v dir1"
     "mkdir -v dir2"
@@ -26,9 +25,9 @@ respuetas=(
     "cd dir2"
     "cd -"
     "cd"
-    )
+    
 
-comandos=(     
+comandos=    
 "pwd"     
 "mkdir -v dir1"     
 "mkdir -v dir2"     
@@ -37,9 +36,9 @@ comandos=(
 "ls"     
 "ls -R"     
 "cd dir2; pwd"
-)
 
-pistas=(     
+
+pistas=    
 "Pista: Imprime el directorio de trabajo"     
 "Pista: Crea el directorio de forma \"verbose\""     
 "Pista: Crea el directorio de forma \"verbose\""     
@@ -50,4 +49,4 @@ pistas=(
 "Pista: Usa rutas relativas."     
 "Pista: Usa el comando para cambiar de directorio y solo un\      caracter."     
 "Pista: Usa el comando para cambiar de directorio con el argumento\         por default, no con ~."     
-)
+
